bb14bb9227c6cd8968a05123f305ac495a377110
D Raolane <222417961@tut4life.ac.za>
2025-09-10 16:36:57 +0200

    Finish login route please

--- routes/login.js
+++ routes/login.js
@@ -11,4 +11,24 @@ router.post('/login', async (req,res,next) =>{
 
     let user = null;
     let role = null;
+
+    user = await Citizen.findOne({where: {email}})
+    if(user) role = "citizen";
+
+    if(!user){
+        user = await Admin.findOne({where: {email}})
+        if(user) role = "admin";
+    }
+    if(!user){
+        user = await MunicipalPersonnel.findOne({where: {email}})
+        if(user) role = "municipal"
+    }
+
+    if(!user){
+        res.sendStatus(401).json({error: 'Invalid email or password'})
+    }
+
+    const token = jwt.sign({
+        id: user.citizen_id || user.admin_id || user.municipality_id, role, process.env.JWT_SECRET, {expires}
+    })
 })
